59fa25671bfae3e2d2cd051a1fb0052dcd5ff94a
AMK1215 <aungmyokhaing1215@gmail.com>
2025-03-14 09:59:29 +0630

    first-init

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import Search from './components/Search';
+import { useState } from 'react';
 
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  });
-
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div className='card'>
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='Hero Banner' />
+          <h1>
+            Find <span className='text-gradient'>Movies</span> you'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          <h1 className='text-white'>
+            {searchTerm ? `Results for "${searchTerm}"` : 'Popular Movies'}
+          </h1>
+          <p className='text-white'>
+            Discover new films, TV shows, and more by entering your favorite
+            keywords. Or use our pre-populated suggestions.
+          </p>
+        </header>
+      </div>
+    </main>
   );
 };
-
-function App() {
-  return (
-    <div className='card-container'>
-      <Card title='Star Wars' />
-      <Card title='Avatars 1' />
-      <Card title='The Lion King 1' />
-    </div>
-  );
-}
 
 export default App;
